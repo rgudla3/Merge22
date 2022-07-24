9bf2c7fdec0cc14e82aa1f1667a875d24df22664
rgudla3 <rgudla3@gatech.edu>
2022-07-23 21:42:41 -0400

    configured pop up alert for plastic bottle option

--- app/src/main/java/com/example/merge2022/PlasticBottleScreen.java
+++ app/src/main/java/com/example/merge2022/PlasticBottleScreen.java
@@ -1,10 +1,13 @@
 package com.example.merge2022;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class PlasticBottleScreen extends AppCompatActivity {
@@ -35,6 +38,17 @@ public class PlasticBottleScreen extends AppCompatActivity {
             public void onClick(View view) {
                 yesUnselected1 = findViewById(R.id.yesSelected1);
                 yesUnselected1.setVisibility(View.VISIBLE);
+                AlertDialog.Builder builder = new AlertDialog.Builder(PlasticBottleScreen.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("Your bottle is empty!");
+                builder.setMessage("You may proceed. In the future, be sure to dump out the contents before recycling.");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) { }
+                });
+                builder.show();
             }
         });
         yes2.setOnClickListener(new View.OnClickListener() {
@@ -42,6 +56,18 @@ public class PlasticBottleScreen extends AppCompatActivity {
             public void onClick(View view) {
                 yesUnselected2 = findViewById(R.id.yesSelected2);
                 yesUnselected2.setVisibility(View.VISIBLE);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(PlasticBottleScreen.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("You have your bottle cap!");
+                builder.setMessage("Please put the bottle cap back on the empty container.");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) { }
+                });
+                builder.show();
             }
         });
         no1.setOnClickListener(new View.OnClickListener() {
@@ -49,6 +75,18 @@ public class PlasticBottleScreen extends AppCompatActivity {
             public void onClick(View view) {
                 noUnselected1 = findViewById(R.id.noSelected1);
                 noUnselected1.setVisibility(View.VISIBLE);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(PlasticBottleScreen.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("Your bottle is not empty!");
+                builder.setMessage("Please make sure to dump out the contents before recycling.");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) { }
+                });
+                builder.show();
             }
         });
         no2.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +94,18 @@ public class PlasticBottleScreen extends AppCompatActivity {
             public void onClick(View view) {
                 noUnselected2 = findViewById(R.id.noSelected2);
                 noUnselected2.setVisibility(View.VISIBLE);
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(PlasticBottleScreen.this);
+
+                builder.setCancelable(true);
+                builder.setTitle("You don't have your bottle cap!");
+                builder.setMessage("You may proceed. In the future, be sure to screw on the cap of your bottle before recycling.");
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) { }
+                });
+                builder.show();
             }
         });
     }
